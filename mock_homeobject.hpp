40cf430eb8060c36f35e5bdee044140c0cabed6c
bszmyd <bszmyd@ebay.com>
2023-07-25 12:31:09 -0600

    Add shard management fields. (#14)

--- mock_homeobject.hpp
+++ mock_homeobject.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <functional>
 #include <memory>
 #include <mutex>
 #include <set>
+#include <utility>
 
 #include <sisl/flip/flip.hpp>
 #include "mock_blob_manager.hpp"
@@ -10,18 +12,46 @@
 #include "mock_shard_manager.hpp"
 
 namespace homeobject {
+struct BlobRoute {
+    shard_id shard;
+    blob_id blob;
+};
+
+inline std::string toString(BlobRoute const& r) { return fmt::format("{}:{}", r.shard, r.blob); }
+
+inline bool operator<(BlobRoute const& lhs, BlobRoute const& rhs) { return toString(lhs) < toString(rhs); }
+
+inline bool operator==(BlobRoute const& lhs, BlobRoute const& rhs) { return toString(lhs) == toString(rhs); }
+} // namespace homeobject
+
+template <>
+struct std::hash< homeobject::BlobRoute > {
+    std::size_t operator()(homeobject::BlobRoute const& r) const noexcept {
+        return std::hash< std::string >()(homeobject::toString(r));
+    }
+};
+
+namespace homeobject {
 
 class MockHomeObject : public BlobManager,
                        public PGManager,
                        public ShardManager,
                        public std::enable_shared_from_this< MockHomeObject > {
+    /// This simulates the MetaBlkSvc thats used within real HomeObject
     std::mutex _pg_lock;
-    std::set< PGInfo > _pgs;
+    std::map< pg_id, std::pair< PGInfo, std::unordered_set< shard_id > > > _pg_map;
+    ///
 
+    /// This simulates the IndexSvc thats used within real HomeObject
+    std::mutex _shard_lock;
+    std::map< shard_id, ShardInfo > _shards;
+    ///
+
+    /// Simulates the Shard=>Chunk mapping in IndexSvc *and* DataSvc BlkId=>Data
     mutable std::mutex _data_lock;
-    std::unordered_map< blob_id, Blob > _in_memory_disk;
-    std::unordered_set< shard_id > _shards;
+    std::unordered_map< BlobRoute, Blob > _in_memory_disk;
     blob_id _cur_blob_id{0};
+    ///
 
 public:
     MockHomeObject() = default;
--- mock_pg_manager.cpp
+++ mock_pg_manager.cpp
@@ -6,7 +6,7 @@ namespace homeobject {
 void MockHomeObject::create_pg(PGInfo const& pg_info, PGManager::ok_cb cb) {
     LOGINFO("Creating PG: [{}] of [{}] members", pg_info.id, pg_info.members.size());
     auto lg = std::scoped_lock(_pg_lock);
-    _pgs.insert(pg_info);
+    _pg_map.insert(std::make_pair(pg_info.id, std::make_pair(pg_info, std::unordered_set< shard_id >())));
     cb(PGError::OK);
 }
 void MockHomeObject::replace_member(pg_id id, peer_id const& old_member, PGMember const& new_member,
@@ -15,13 +15,13 @@ void MockHomeObject::replace_member(pg_id id, peer_id const& old_member, PGMembe
     auto err = PGError::OK;
     {
         auto lg = std::scoped_lock(_pg_lock);
-        auto pg_it = _pgs.end();
-        if (pg_it = _pgs.find(id); _pgs.end() == pg_it) {
+        auto pg_it = _pg_map.end();
+        if (pg_it = _pg_map.find(id); _pg_map.end() == pg_it) {
             err = PGError::UNKNOWN_PG;
-        } else if (0 == pg_it->members.erase(old_member)) {
+        } else if (auto& members = pg_it->second.first.members; 0 == members.erase(old_member)) {
             err = PGError::UNKNOWN_PEER;
         } else {
-            auto [it, happened] = pg_it->members.insert(new_member);
+            auto [it, happened] = members.insert(new_member);
             if (!happened) {
                 LOGERROR("Already have this member [{}] in [pg={}]", to_string(new_member.id), id);
                 err = PGError::BAD_ARGUMENT;
