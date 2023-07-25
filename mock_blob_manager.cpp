a66f3dd0117ccbb2c3576d555183e2b5a5d87576
Ravi Akella email = raakella@ebay.com <raakella@sdsbuild07>
2023-07-24 17:35:22 -0700

    release lock before sending grpc response

--- mock_blob_manager.cpp
+++ mock_blob_manager.cpp
@@ -1,55 +1,75 @@
 #include "mock_homeobject.hpp"
 
 namespace homeobject {
-static constexpr uint32_t disk_latency = 15; // ms
+using namespace std::chrono_literals;
+constexpr auto disk_latency = 15ms;
 
 void MockHomeObject::put(shard_id shard, Blob const& blob, id_cb cb) {
     std::thread([this, shard, blob, cb]() {
-        std::this_thread::sleep_for(std::chrono::milliseconds(disk_latency));
-        auto lg = std::scoped_lock(_data_lock);
-        _shards.insert(shard);
-        _in_memory_disk.emplace(_cur_blob_id, blob);
-        cb(_cur_blob_id++, std::nullopt);
+        std::this_thread::sleep_for(disk_latency);
+        blob_id id;
+        {
+            auto lg = std::scoped_lock(_data_lock);
+            _shards.insert(shard);
+            id = _cur_blob_id;
+            auto [it, happened] = _in_memory_disk.emplace(id, blob);
+            if (happened) { _cur_blob_id++; }
+        }
+        cb(id, std::nullopt);
     }).detach();
 }
 
 void MockHomeObject::get(shard_id shard, blob_id const& blob, uint64_t off, uint64_t len, get_cb cb) const {
     std::thread([this, shard, blob, cb]() {
-        std::this_thread::sleep_for(std::chrono::milliseconds(disk_latency));
-        auto lg = std::scoped_lock(_data_lock);
-        if (auto const it = _shards.find(shard); it == _shards.end()) {
-            cb(BlobError::UNKNOWN_SHARD, std::nullopt);
-            return;
-        }
-        auto it = _in_memory_disk.end();
-        if (it = _in_memory_disk.find(blob); it == _in_memory_disk.end()) {
-            cb(BlobError::UNKNOWN_BLOB, std::nullopt);
-            return;
-        }
+        BlobError err = BlobError::OK;
+        Blob ret;
+        [&] {
+            std::this_thread::sleep_for(disk_latency);
+            auto lg = std::scoped_lock(_data_lock);
+            if (auto const it = _shards.find(shard); it == _shards.end()) {
+                err = BlobError::UNKNOWN_SHARD;
+                return;
+            }
+            auto it = _in_memory_disk.end();
+            if (it = _in_memory_disk.find(blob); it == _in_memory_disk.end()) {
+                err = BlobError::UNKNOWN_BLOB;
+                return;
+            }
 
-        auto const& read_blob = it->second;
-        auto ret = Blob{sisl::io_blob(read_blob.body.size), read_blob.user_key, read_blob.object_off};
-        std::memcpy(reinterpret_cast< void* >(ret.body.bytes), reinterpret_cast< void* >(read_blob.body.bytes),
-                    ret.body.size);
-        cb(ret, std::nullopt);
+            auto const& read_blob = it->second;
+            ret.body = sisl::io_blob(read_blob.body.size);
+            ret.user_key = read_blob.user_key;
+            ret.object_off = read_blob.object_off;
+            std::memcpy(reinterpret_cast< void* >(ret.body.bytes), reinterpret_cast< void* >(read_blob.body.bytes),
+                        ret.body.size);
+        }();
+
+        if (err == BlobError::OK) {
+            cb(ret, std::nullopt);
+        } else {
+            cb(err, std::nullopt);
+        }
     }).detach();
 }
 void MockHomeObject::del(shard_id shard, blob_id const& blob, BlobManager::ok_cb cb) {
     std::thread([this, shard, blob, cb]() {
-        auto lg = std::scoped_lock(_data_lock);
-        if (auto const it = _shards.find(shard); it == _shards.end()) {
-            cb(BlobError::UNKNOWN_SHARD, std::nullopt);
-            return;
-        }
-        auto it = _in_memory_disk.end();
-        if (it = _in_memory_disk.find(blob); it == _in_memory_disk.end()) {
-            cb(BlobError::UNKNOWN_BLOB, std::nullopt);
-            return;
-        }
+        BlobError err = BlobError::OK;
+        [&] {
+            auto lg = std::scoped_lock(_data_lock);
+            if (auto const it = _shards.find(shard); it == _shards.end()) {
+                err = BlobError::UNKNOWN_SHARD;
+                return;
+            }
+            auto it = _in_memory_disk.end();
+            if (it = _in_memory_disk.find(blob); it == _in_memory_disk.end()) {
+                err = BlobError::UNKNOWN_BLOB;
+                return;
+            }
 
-        _shards.erase(shard);
-        _in_memory_disk.erase(blob);
-        cb(BlobError::OK, std::nullopt);
+            _in_memory_disk.erase(blob);
+        }();
+
+        cb(err, std::nullopt);
     }).detach();
 }
 
