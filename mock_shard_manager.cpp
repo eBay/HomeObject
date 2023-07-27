bf209b27d492707d1dedc7d361d1d24980fa1849
Brian Szmyd <bszmyd@ebay.com>
2023-07-27 12:05:42 -0600

    Move re-usable pieces (for testing) to common lib.

--- mock_shard_manager.cpp
+++ mock_shard_manager.cpp
@@ -14,26 +14,17 @@ static uint64_t get_current_timestamp() {
 
 void MockHomeObject::create_shard(pg_id pg_owner, uint64_t size_bytes, ShardManager::id_cb cb) {
     LOGINFO("Creating Shard: in Pg [{}] of Size [{}b]", pg_owner, size_bytes);
-    auto err = ShardError::OK;
+    auto err = ShardError::UNKNOWN_PG;
     auto id = shard_id{0};
     {
         auto lg = std::scoped_lock(_pg_lock, _shard_lock);
-        auto pg_it = _pg_map.find(pg_owner);
-        if (_pg_map.end() == pg_it) {
-            if (0 < SISL_OPTIONS.count("m1_demo")) {
-                auto const pg_info = PGInfo(pg_owner);
-                auto [it, happened] =
-                    _pg_map.insert(std::make_pair(pg_owner, std::make_pair(pg_info, std::unordered_set< shard_id >())));
-                pg_it = it;
-            } else
-                err = ShardError::UNKNOWN_PG;
-        }
-        if (ShardError::OK == err) {
+        if (auto pg_it = _pg_map.find(pg_owner); _pg_map.end() != pg_it) {
             id = _cur_shard_id++;
             pg_it->second.second.emplace(id);
-            _shards.emplace(std::make_pair(
+            auto [_, happened] = _shards.emplace(std::make_pair(
                 id,
                 ShardInfo{id, pg_owner, ShardInfo::State::OPEN, 1024 * 1024 * 1024, get_current_timestamp(), 0, 0, 0}));
+            err = happened ? ShardError::OK : ShardError::UNKNOWN;
         }
     }
     if (!cb) return;
@@ -46,14 +37,12 @@ void MockHomeObject::create_shard(pg_id pg_owner, uint64_t size_bytes, ShardMana
 
 void MockHomeObject::get_shard(shard_id id, ShardManager::info_cb cb) const {
     auto info = ShardInfo();
-    auto err = ShardError::OK;
+    auto err = ShardError::UNKNOWN_SHARD;
     {
         auto lg = std::scoped_lock(_pg_lock, _shard_lock);
-        if (auto shard_it = _shards.find(id); _shards.end() == shard_it) {
-            LOGERROR("Could not find Shard: [shard={}] on this instance!", id);
-            err = ShardError::UNKNOWN_SHARD;
-        } else {
+        if (auto shard_it = _shards.find(id); _shards.end() != shard_it) {
             info = shard_it->second;
+            err = ShardError::OK;
         }
     }
 
@@ -67,13 +56,11 @@ void MockHomeObject::get_shard(shard_id id, ShardManager::info_cb cb) const {
 
 void MockHomeObject::list_shards(pg_id id, ShardManager::list_cb cb) const {
     auto info = std::vector< ShardInfo >();
-    auto err = ShardError::OK;
+    auto err = ShardError::UNKNOWN_PG;
     {
         auto lg = std::scoped_lock(_pg_lock, _shard_lock);
-        if (auto pg_it = _pg_map.find(id); _pg_map.end() == pg_it) {
-            LOGERROR("Could not find Pg: [pg={}] on this instance!", id);
-            err = ShardError::UNKNOWN_PG;
-        } else {
+        if (auto pg_it = _pg_map.find(id); _pg_map.end() != pg_it) {
+            err = ShardError::OK;
             for (auto const& shard_id : pg_it->second.second) {
                 auto shard_it = _shards.find(shard_id);
                 RELEASE_ASSERT(_shards.end() != shard_it, "Missing Shard [{}]!", shard_id);
@@ -92,15 +79,13 @@ void MockHomeObject::list_shards(pg_id id, ShardManager::list_cb cb) const {
 
 void MockHomeObject::seal_shard(shard_id id, ShardManager::info_cb cb) {
     auto info = ShardInfo();
-    auto err = ShardError::OK;
+    auto err = ShardError::UNKNOWN_SHARD;
     {
         auto lg = std::scoped_lock(_pg_lock, _shard_lock);
-        if (auto shard_it = _shards.find(id); _shards.end() == shard_it) {
-            LOGERROR("Could not find Shard: [shard={}] on this instance!", id);
-            err = ShardError::UNKNOWN_SHARD;
-        } else {
+        if (auto shard_it = _shards.find(id); _shards.end() != shard_it) {
             shard_it->second.state = ShardInfo::State::SEALED;
             info = shard_it->second;
+            err = ShardError::OK;
         }
     }
 
