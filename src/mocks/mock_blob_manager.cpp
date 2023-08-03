b8cdce3a5fc1f24e718846486071ca409aa82c71
Brian Szmyd <brian.szmyd@protonmail.com>
2023-08-03 15:40:16 -0600

    Remove error-prone code in bugs with macro. (#4)

--- src/mocks/mock_blob_manager.cpp
+++ src/mocks/mock_blob_manager.cpp
@@ -4,78 +4,66 @@ namespace homeobject {
 using namespace std::chrono_literals;
 constexpr auto disk_latency = 15ms;
 
-#define CB_IF_DEFINED(ret, e)                                                                                          \
-    if (!cb) return;                                                                                                   \
-    auto const temp_err = (e);                                                                                         \
-    if (temp_err == BlobError::OK) {                                                                                   \
-        cb((ret), std::nullopt);                                                                                       \
-    } else {                                                                                                           \
-        cb(temp_err, std::nullopt);                                                                                    \
-    }
+#define WITH_DATA_LOCKS(e)                                                                                             \
+    auto err = (e);                                                                                                    \
+    {                                                                                                                  \
+        auto lg = std::scoped_lock(_shard_lock, _data_lock);
+
+#define CB_FROM_DATA_LOCKS(ret, e, ok)                                                                                 \
+    }                                                                                                                  \
+    CB_IF_DEFINED(ret, e, ok)
 
 void MockHomeObject::put(shard_id shard, Blob&& blob, BlobManager::id_cb const& cb) {
     std::thread([this, shard, mblob = std::move(blob), cb]() mutable {
         std::this_thread::sleep_for(disk_latency);
         blob_id id;
-        auto err = BlobError::UNKNOWN_SHARD;
-        {
-            // Need both locks to prevent sealing while writting
-            auto lg = std::scoped_lock(_shard_lock, _data_lock);
-            if (0 != _shards.count(shard)) {
-                err = BlobError::OK;
-                id = _cur_blob_id;
-                if (auto [it, happened] = _in_memory_disk.emplace(BlobRoute{shard, id}, std::move(mblob)); happened) {
-                    _cur_blob_id++;
-                }
+        WITH_DATA_LOCKS(BlobError::UNKNOWN_SHARD)
+        if (0 != _shards.count(shard)) {
+            err = BlobError::OK;
+            id = _cur_blob_id;
+            if (auto [it, happened] = _in_memory_disk.emplace(BlobRoute{shard, id}, std::move(mblob)); happened) {
+                _cur_blob_id++;
             }
         }
-        CB_IF_DEFINED(id, err);
+        CB_FROM_DATA_LOCKS(id, err, BlobError::OK)
     }).detach();
 }
 
 void MockHomeObject::get(shard_id shard, blob_id const& id, uint64_t off, uint64_t len,
                          BlobManager::get_cb const& cb) const {
     std::thread([this, shard, id, cb]() {
-        BlobError err = BlobError::UNKNOWN_SHARD;
-        Blob blob;
-
+        BlobError serr = BlobError::UNKNOWN_SHARD;
         // Only need to lookup shard with _shard_lock for READs, okay to seal while reading
         {
             auto lg = std::scoped_lock(_shard_lock);
-            if (auto const it = _shards.find(shard); it != _shards.end()) { err = BlobError::OK; }
+            if (auto const it = _shards.find(shard); it != _shards.end()) { serr = BlobError::OK; }
+        }
+        if (BlobError::OK != serr) {
+            CB_IF_DEFINED(serr, serr, BlobError::OK);
+            return;
         }
 
-        if (BlobError::OK == err) {
-            [&] {
-                std::this_thread::sleep_for(disk_latency);
-                auto lg = std::scoped_lock(_data_lock);
-                auto it = _in_memory_disk.end();
-                if (it = _in_memory_disk.find(BlobRoute{shard, id}); it == _in_memory_disk.end()) {
-                    err = BlobError::UNKNOWN_BLOB;
-                    return;
-                }
-
-                auto const& read_blob = it->second;
-                blob.body = std::make_unique< sisl::byte_array_impl >(read_blob.body->size);
-                blob.object_off = read_blob.object_off;
-                blob.user_key = read_blob.user_key;
-                std::memcpy(blob.body->bytes, read_blob.body->bytes, blob.body->size);
-            }();
+        std::this_thread::sleep_for(disk_latency);
+        Blob blob;
+        WITH_DATA_LOCKS(BlobError::UNKNOWN_BLOB)
+        if (auto it = _in_memory_disk.find(BlobRoute{shard, id}); it != _in_memory_disk.end()) {
+            auto const& read_blob = it->second;
+            blob.body = std::make_unique< sisl::byte_array_impl >(read_blob.body->size);
+            blob.object_off = read_blob.object_off;
+            blob.user_key = read_blob.user_key;
+            std::memcpy(blob.body->bytes, read_blob.body->bytes, blob.body->size);
+            err = BlobError::OK;
         }
-        CB_IF_DEFINED(std::move(blob), err);
+        CB_FROM_DATA_LOCKS(std::move(blob), err, BlobError::OK)
     }).detach();
 }
 void MockHomeObject::del(shard_id shard, blob_id const& blob, BlobManager::ok_cb const& cb) {
     std::thread([this, shard, blob, cb]() {
-        BlobError err = BlobError::UNKNOWN_SHARD;
-        [&] {
-            /// Take both locks so we don't seal while deleting the BLOB.
-            auto lg = std::scoped_lock(_shard_lock, _data_lock);
-            if (auto const it = _shards.find(shard); it != _shards.end()) {
-                err = (0 < _in_memory_disk.erase(BlobRoute{shard, blob})) ? BlobError::OK : BlobError::UNKNOWN_BLOB;
-            }
-        }();
-        CB_IF_DEFINED(err, BlobError::OK);
+        WITH_DATA_LOCKS(BlobError::UNKNOWN_SHARD)
+        if (auto const it = _shards.find(shard); it != _shards.end()) {
+            err = (0 < _in_memory_disk.erase(BlobRoute{shard, blob})) ? BlobError::OK : BlobError::UNKNOWN_BLOB;
+        }
+        CB_FROM_DATA_LOCKS(err, BlobError::OK, BlobError::OK)
     }).detach();
 }
 
--- src/mocks/mock_shard_manager.cpp
+++ src/mocks/mock_shard_manager.cpp
@@ -8,93 +8,66 @@ static uint64_t get_current_timestamp() {
     return timestamp;
 }
 
+#define WITH_SHARD_LOCKS(e)                                                                                            \
+    auto err = (e);                                                                                                    \
+    {                                                                                                                  \
+        auto lg = std::scoped_lock(_pg_lock, _shard_lock);
+
+#define CB_FROM_SHARD_LOCKS(ret, e, ok)                                                                                \
+    }                                                                                                                  \
+    CB_IF_DEFINED(ret, e, ok)
+
 void MockHomeObject::create_shard(pg_id pg_owner, uint64_t size_bytes, ShardManager::info_cb const& cb) {
     LOGINFO("Creating Shard: in Pg [{}] of Size [{}b]", pg_owner, size_bytes);
-    auto err = ShardError::UNKNOWN_PG;
     auto ret = ShardInfo();
-    {
-        auto lg = std::scoped_lock(_pg_lock, _shard_lock);
-        if (auto pg_it = _pg_map.find(pg_owner); _pg_map.end() != pg_it) {
-            auto const id = _cur_shard_id++;
-            pg_it->second.second.emplace(id);
-            if (auto [it, happened] =
-                    _shards.emplace(std::make_pair(id,
-                                                   ShardInfo{id, pg_owner, ShardInfo::State::OPEN, 1024 * 1024 * 1024,
-                                                             get_current_timestamp(), 0, 0, 0}));
-                _shards.end() != it) {
-                err = happened ? ShardError::OK : ShardError::UNKNOWN;
-                ret = it->second;
-            }
+    WITH_SHARD_LOCKS(ShardError::UNKNOWN_PG);
+    if (auto pg_it = _pg_map.find(pg_owner); _pg_map.end() != pg_it) {
+        auto const id = _cur_shard_id++;
+        pg_it->second.second.emplace(id);
+        if (auto [it, happened] = _shards.emplace(std::make_pair(
+                id,
+                ShardInfo{id, pg_owner, ShardInfo::State::OPEN, 1024 * 1024 * 1024, get_current_timestamp(), 0, 0, 0}));
+            _shards.end() != it) {
+            err = happened ? ShardError::OK : ShardError::UNKNOWN;
+            ret = it->second;
         }
     }
-    if (!cb) return;
-    if (err == ShardError::OK) {
-        cb(ret, std::nullopt);
-    } else {
-        cb(err, std::nullopt);
-    }
+    CB_FROM_SHARD_LOCKS(ret, err, ShardError::OK)
 }
 
 void MockHomeObject::get_shard(shard_id id, ShardManager::info_cb const& cb) const {
     auto info = ShardInfo();
-    auto err = ShardError::UNKNOWN_SHARD;
-    {
-        auto lg = std::scoped_lock(_pg_lock, _shard_lock);
-        if (auto shard_it = _shards.find(id); _shards.end() != shard_it) {
-            info = shard_it->second;
-            err = ShardError::OK;
-        }
+    WITH_SHARD_LOCKS(ShardError::UNKNOWN_SHARD)
+    if (auto shard_it = _shards.find(id); _shards.end() != shard_it) {
+        info = shard_it->second;
+        err = ShardError::OK;
     }
-
-    if (!cb) return;
-    if (err == ShardError::OK) {
-        cb(info, std::nullopt);
-    } else {
-        cb(err, std::nullopt);
-    }
+    CB_FROM_SHARD_LOCKS(info, err, ShardError::OK)
 }
 
 void MockHomeObject::list_shards(pg_id id, ShardManager::list_cb const& cb) const {
     auto info = std::vector< ShardInfo >();
-    auto err = ShardError::UNKNOWN_PG;
-    {
-        auto lg = std::scoped_lock(_pg_lock, _shard_lock);
-        if (auto pg_it = _pg_map.find(id); _pg_map.end() != pg_it) {
-            err = ShardError::OK;
-            for (auto const& shard_id : pg_it->second.second) {
-                auto shard_it = _shards.find(shard_id);
-                RELEASE_ASSERT(_shards.end() != shard_it, "Missing Shard [{}]!", shard_id);
-                info.push_back(shard_it->second);
-            }
+    WITH_SHARD_LOCKS(ShardError::UNKNOWN_PG)
+    if (auto pg_it = _pg_map.find(id); _pg_map.end() != pg_it) {
+        err = ShardError::OK;
+        for (auto const& shard_id : pg_it->second.second) {
+            auto shard_it = _shards.find(shard_id);
+            RELEASE_ASSERT(_shards.end() != shard_it, "Missing Shard [{}]!", shard_id);
+            info.push_back(shard_it->second);
         }
     }
-
-    if (!cb) return;
-    if (err == ShardError::OK) {
-        cb(info, std::nullopt);
-    } else {
-        cb(err, std::nullopt);
-    }
+    CB_FROM_SHARD_LOCKS(info, err, ShardError::OK)
 }
 
 void MockHomeObject::seal_shard(shard_id id, ShardManager::info_cb const& cb) {
     auto info = ShardInfo();
-    auto err = ShardError::UNKNOWN_SHARD;
-    {
-        auto lg = std::scoped_lock(_pg_lock, _shard_lock);
-        if (auto shard_it = _shards.find(id); _shards.end() != shard_it) {
-            shard_it->second.state = ShardInfo::State::SEALED;
-            info = shard_it->second;
-            err = ShardError::OK;
-        }
+    WITH_SHARD_LOCKS(ShardError::UNKNOWN_SHARD)
+    if (auto shard_it = _shards.find(id); _shards.end() != shard_it) {
+        shard_it->second.state = ShardInfo::State::SEALED;
+        info = shard_it->second;
+        err = ShardError::OK;
     }
-
-    if (!cb) return;
-    if (err == ShardError::OK) {
-        cb(info, std::nullopt);
-    } else {
-        cb(err, std::nullopt);
-    }
+    CB_FROM_SHARD_LOCKS(info, err, ShardError::OK)
 }
 
 } // namespace homeobject
