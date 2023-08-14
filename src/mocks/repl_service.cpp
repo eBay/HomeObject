3012e97ae7e3114dda68a83e917b9e9eff0e40d1
Brian Szmyd <bszmyd@ebay.com>
2023-08-14 09:35:45 -0700

    Use futures.

--- src/mocks/repl_service.cpp
+++ src/mocks/repl_service.cpp
@@ -7,24 +7,25 @@ public:
     ~MockReplicationService() override = default;
 
     std::variant< rs_ptr_t, ReplServiceError > create_replica_set(std::string_view group_id) override;
-    std::variant< rs_ptr_t, ReplServiceError > lookup_replica_set(std::string_view group_id) const override;
-    ReplServiceError replace_member(std::string_view group_id, std::string_view member_out,
-                                    std::string_view member_in) const override;
-    void iterate_replica_sets(each_set_cb cb) const override;
+    std::variant< rs_ptr_t, ReplServiceError > get_replica_set(std::string_view group_id) const override;
+    folly::SemiFuture< ReplServiceError > replace_member(std::string_view group_id, std::string_view member_out,
+                                                         std::string_view member_in) const override;
+    void iterate_replica_sets(std::function< void(const rs_ptr_t&) > cb) const override;
 };
 
 std::variant< rs_ptr_t, ReplServiceError > MockReplicationService::create_replica_set(std::string_view group_id) {
     return ReplServiceError::CANCELLED;
 }
 
-std::variant< rs_ptr_t, ReplServiceError > MockReplicationService::lookup_replica_set(std::string_view group_id) const {
+std::variant< rs_ptr_t, ReplServiceError > MockReplicationService::get_replica_set(std::string_view group_id) const {
     return ReplServiceError::CANCELLED;
 }
-ReplServiceError MockReplicationService::replace_member(std::string_view group_id, std::string_view member_out,
-                                                        std::string_view member_in) const {
-    return ReplServiceError::CANCELLED;
+folly::SemiFuture< ReplServiceError > MockReplicationService::replace_member(std::string_view group_id,
+                                                                             std::string_view member_out,
+                                                                             std::string_view member_in) const {
+    return folly::makeSemiFuture(ReplServiceError::CANCELLED);
 }
 
-void MockReplicationService::iterate_replica_sets(each_set_cb) const {}
+void MockReplicationService::iterate_replica_sets(std::function< void(const rs_ptr_t&) > cb) const {}
 
 } // namespace home_replication
--- src/mocks/repl_service.hpp
+++ src/mocks/repl_service.hpp
@@ -5,8 +5,9 @@
 #include <string_view>
 #include <variant>
 
-#include "repl_common.hpp"
+#include <folly/futures/Future.h>
 
+#include "repl_common.hpp"
 #include "repl_set.hpp"
 
 namespace nuraft {
@@ -19,22 +20,27 @@ using rs_ptr_t = std::shared_ptr< ReplicaSet >;
 
 using ReplServiceError = nuraft::cmd_result_code;
 
-class ReplicationService {
-public:
-    using lookup_member_cb = std::function< std::string(uuid const&) >;
-    using each_set_cb = std::function< void(const rs_ptr_t&) >;
-    using on_replica_set_init_t = std::function< std::shared_ptr< nuraft::state_machine >(const rs_ptr_t& rs) >;
+class ReplicatedServer {
+    virtual ~ReplicatedServer() = default;
 
-    virtual ~ReplicationService() = default;
-
-    virtual std::variant< rs_ptr_t, ReplServiceError > create_replica_set(std::string_view group_id) = 0;
-    virtual std::variant< rs_ptr_t, ReplServiceError > lookup_replica_set(std::string_view group_id) const = 0;
-    virtual ReplServiceError replace_member(std::string_view group_id, std::string_view member_out,
-                                            std::string_view member_in) const = 0;
-    virtual void iterate_replica_sets(each_set_cb cb) const = 0;
+    /// Map HomeObject instance to
+    virtual folly::SemiFuture< std::string > member_address(uuid const&) const = 0;
 };
 
-extern std::shared_ptr< ReplicationService > create_repl_service(ReplicationService::on_replica_set_init_t cb,
-                                                                 ReplicationService::lookup_member_cb);
+class ReplicationService {
+public:
+    virtual ~ReplicationService() = default;
+
+    /// Sync APIs
+    virtual std::variant< rs_ptr_t, ReplServiceError > create_replica_set(std::string_view group_id) = 0;
+    virtual std::variant< rs_ptr_t, ReplServiceError > get_replica_set(std::string_view group_id) const = 0;
+    virtual void iterate_replica_sets(std::function< void(const rs_ptr_t&) > cb) const = 0;
+
+    /// Async APIs
+    virtual folly::SemiFuture< ReplServiceError > replace_member(std::string_view group_id, std::string_view member_out,
+                                                                 std::string_view member_in) const = 0;
+};
+
+extern std::shared_ptr< ReplicationService > create_repl_service(ReplicatedServer& server);
 
 } // namespace home_replication
