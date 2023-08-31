fedc8352bc95de9bb5f7f897543b61f2fa6a5ef6
Brian Szmyd <bszmyd@ebay.com>
2023-08-31 16:04:12 -0600

    Wrong lock type taken.

--- src/mocks/mock_blob_manager.cpp
+++ src/mocks/mock_blob_manager.cpp
@@ -11,28 +11,28 @@ BlobManager::AsyncResult< blob_id > MockHomeObject::put(shard_id shard, Blob&& b
         .thenValue(
             [this, shard_info = e.value(), blob = std::move(blob)](auto) mutable -> BlobManager::Result< blob_id > {
                 // Simulate ::commit()
-                auto new_pba = _in_memory_disk.end();
+                auto new_blkid = _in_memory_disk.end();
                 blob_id new_id;
                 {
                     // Should be lock-free append only stucture
                     auto lg = std::scoped_lock(_data_lock);
                     LOGDEBUGMOD(homeobject, "Writing Blob {}", _in_memory_disk.size());
                     new_id = _in_memory_disk.size();
-                    new_pba = _in_memory_disk.insert(new_pba, std::move(blob));
+                    new_blkid = _in_memory_disk.insert(new_blkid, std::move(blob));
                 }
                 {
                     auto lg = std::scoped_lock(_index_lock);
-                    auto [_, happened] = _in_memory_index.try_emplace(BlobRoute{shard_info.id, new_id}, new_pba);
+                    auto [_, happened] = _in_memory_index.try_emplace(BlobRoute{shard_info.id, new_id}, new_blkid);
                     RELEASE_ASSERT(happened, "Generated duplicate BlobRoute!");
                 }
                 return new_id;
             });
 }
 
-BlobManager::AsyncResult< MockHomeObject::pba > MockHomeObject::_get_pba(ShardInfo const& shard, blob_id id) const {
+BlobManager::AsyncResult< MockHomeObject::blkid > MockHomeObject::_get_blkid(ShardInfo const& shard, blob_id id) const {
     return folly::makeSemiFuture()
         .via(folly::getGlobalCPUExecutor())
-        .thenValue([this, route = BlobRoute(shard.id, id)](auto) mutable -> BlobManager::Result< pba > {
+        .thenValue([this, route = BlobRoute(shard.id, id)](auto) mutable -> BlobManager::Result< blkid > {
             auto d_it = _in_memory_disk.cend();
             {
                 auto lg = std::shared_lock(_index_lock);
@@ -43,7 +43,6 @@ BlobManager::AsyncResult< MockHomeObject::pba > MockHomeObject::_get_pba(ShardIn
                 LOGWARNMOD(homeobject, "Blob missing {} during get", route.blob);
                 return folly::makeUnexpected(BlobError::UNKNOWN_BLOB);
             }
-            if (!d_it->body) return folly::makeUnexpected(BlobError::DELETED_BLOB);
             return d_it;
         });
 }
@@ -52,9 +51,11 @@ BlobManager::AsyncResult< Blob > MockHomeObject::get(shard_id shard, blob_id con
     auto e = get_shard(shard);
     if (!e) return folly::makeUnexpected(BlobError::UNKNOWN_SHARD);
 
-    return _get_pba(e.value(), id).deferValue([](auto const& e) -> BlobManager::Result< Blob > {
+    auto const& shard_info = e.value();
+    return _get_blkid(shard_info, id).deferValue([](auto const& e) -> BlobManager::Result< Blob > {
         if (!e) return folly::makeUnexpected(e.error());
         auto& read_blob = *(e.value());
+        RELEASE_ASSERT(read_blob.body, "Blob returned with no body!");
         Blob blob;
         blob.body = std::make_unique< sisl::byte_array_impl >(read_blob.body->size);
         blob.object_off = read_blob.object_off;
@@ -64,27 +65,35 @@ BlobManager::AsyncResult< Blob > MockHomeObject::get(shard_id shard, blob_id con
     });
 }
 
-BlobManager::NullAsyncResult MockHomeObject::_del_pba(ShardInfo const& shard, blob_id id) {
-    return folly::makeSemiFuture()
-        .via(folly::getGlobalCPUExecutor())
-        .thenValue([this, route = BlobRoute(shard.id, id)](auto) mutable -> BlobManager::NullResult {
-            {
-                auto lg = std::shared_lock(_index_lock);
-                LOGDEBUGMOD(homeobject, "Looking up Blob {} in set of {}", route.blob, _in_memory_disk.size());
-                if (auto it = _in_memory_index.find(route); _in_memory_index.end() != it) {
-                    _in_memory_index.erase(it);
-                    return folly::Unit();
-                }
-                return folly::makeUnexpected(BlobError::UNKNOWN_BLOB);
-            }
-        });
-}
-
 BlobManager::NullAsyncResult MockHomeObject::del(shard_id shard, blob_id const& id) {
     auto e = get_shard(shard);
     if (!e) return folly::makeUnexpected(BlobError::UNKNOWN_SHARD);
 
-    return _del_pba(e.value(), id);
+    auto const& shard_info = e.value();
+    auto f =
+        folly::makeSemiFuture()
+            .via(folly::getGlobalCPUExecutor())
+            .thenValue([this, route = BlobRoute(shard_info.id, id)](auto) mutable -> BlobManager::Result< blkid > {
+                auto d_it = _in_memory_disk.cend();
+                {
+                    auto lg = std::scoped_lock(_index_lock);
+                    LOGDEBUGMOD(homeobject, "Looking up Blob {} in set of {}", route.blob, _in_memory_index.size());
+                    if (auto it = _in_memory_index.find(route); _in_memory_index.end() != it) {
+                        d_it = it->second;
+                        _in_memory_index.erase(it);
+                    } else
+                        return folly::makeUnexpected(BlobError::UNKNOWN_BLOB);
+                }
+                return d_it;
+            });
+    // TODO We defer GC of the BLOB data to the client response callback, BLOB itself still needs cleanup
+    return BlobManager::AsyncResult< blkid >(std::move(f))
+        .deferValue([this](auto const e) mutable -> BlobManager::NullResult {
+            if (!e) return folly::makeUnexpected(e.error());
+            auto const& d_it = e.value();
+            _in_memory_disk.erase(d_it, d_it)->body.reset();
+            return folly::Unit();
+        });
 }
 
 } // namespace homeobject
--- src/mocks/tests/BlobManagerTest.cpp
+++ src/mocks/tests/BlobManagerTest.cpp
@@ -74,7 +74,7 @@ protected:
 
 TEST_F(BlobManagerFixture, BasicTests) {
     auto calls = std::vector< folly::SemiFuture< folly::Unit > >();
-    for (auto i = _blob_id + _shard.id + 1; (_blob_id + _shard.id + 1) + (8 * Ki) > i; ++i) {
+    for (auto i = _blob_id + _shard.id + 1; (_blob_id + _shard.id + 1) + (10 * Ki) > i; ++i) {
         calls.push_back(m_mock_homeobj->blob_manager()->get(_shard.id, _blob_id).deferValue([](auto const& e) {
             EXPECT_TRUE(!!e);
             e.then([](auto const& blob) {
