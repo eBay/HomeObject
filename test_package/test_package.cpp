700c31cc060e5669e86b2292e7b3830cc9c43d8e
Brian Szmyd <brian.szmyd@protonmail.com>
2023-09-05 09:48:15 -0600

    Remove replication dependency (#39)

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -21,7 +21,6 @@ int main(int argc, char** argv) {
     SISL_OPTIONS_LOAD(argc, argv, logging)
     sisl::logging::SetLogger(std::string(argv[0]));
     spdlog::set_pattern("[%D %T%z] [%^%l%$] [%n] [%t] %v");
-    sisl::logging::SetModuleLogLevel("home_replication", spdlog::level::level_enum::trace);
 
     auto a = std::make_shared<TestApp>();
     homeobject::init_homeobject(a);
