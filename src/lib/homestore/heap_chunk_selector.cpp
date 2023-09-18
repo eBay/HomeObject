9ec3b9c730c6f61c80b9ce391c52618ab2908e05
Jie Yao <jyao3@ebay.com>
2023-09-18 05:05:03 -0700

    use AtomicHashMap

--- src/lib/homestore/heap_chunk_selector.cpp
+++ src/lib/homestore/heap_chunk_selector.cpp
@@ -6,33 +6,71 @@
 
 namespace homeobject {
 
+//https://github.com/facebook/folly/blob/61c11d77eb9a8bdc60f673017fccfbe900125cb6/folly/AtomicHashMap.h#L42
+//AtomichashMap has a max size limit of ~18x initial size.
+//we support max of 64k chunks for now, so 64*1024/18 + 1 = 3641, we use 4000 for sufficiently initial size 
+#define DEFAULT_ATOMICHASHMAP_INITIAL_SIZE 4000
+
+HeapChunkSelector::HeapChunkSelector() : 
+m_pdev_heap_map(PdevHeapMap(DEFAULT_ATOMICHASHMAP_INITIAL_SIZE)), 
+m_pdev_avalable_blk_map(PdevAvalableBlkMap(DEFAULT_ATOMICHASHMAP_INITIAL_SIZE)), 
+m_chunks(DEFAULT_ATOMICHASHMAP_INITIAL_SIZE) {}
+
+HeapChunkSelector::HeapChunkSelector(const uint32_t& pdev_heap_map_initial_size, 
+const uint32_t& pdev_avalable_blk_map_initial_size, const uint32_t& chunk_initial_num) : 
+m_pdev_heap_map(PdevHeapMap(pdev_heap_map_initial_size)), 
+m_pdev_avalable_blk_map(PdevAvalableBlkMap(pdev_avalable_blk_map_initial_size)),
+m_chunks(ChunkMap(chunk_initial_num)) {}
+
+
 //add_chunk may be called in Homeobject(when sealing a shard) or in Homestore(when adding chunks to vdev) concurently
 void HeapChunkSelector::add_chunk(csharedChunk& chunk) {
     VChunk vchunk(chunk);
-    auto&& pdevId = vchunk.get_pdev_id();
-    auto&& [it, ok] = m_pdev_heap_map.try_emplace(pdevId, std::make_shared<std::pair<std::mutex, VChunkHeap>>());
-    
-    {
-        std::lock_guard<std::mutex> l(lock); 
-        if(ok) m_pdev_avalable_blk_map.emplace(pdevId, vchunk.available_blks());
-        else m_pdev_avalable_blk_map[pdevId] += vchunk.available_blks();
-        m_chunks.emplace(chunk);
-    }
+    auto&& pdevID = vchunk.get_pdev_id();
+    auto&& [it, _] = m_pdev_heap_map.emplace(pdevID, std::make_shared<std::pair<std::mutex, VChunkHeap>>());
+    const auto& available_blk_num = vchunk.available_blks();
+    auto&& [ignore, ok] = m_pdev_avalable_blk_map.emplace(pdevID, available_blk_num);
 
-    std::lock_guard<std::mutex> l(it->second->first); 
-    it->second->second.emplace(chunk);
+    if(!ok) {
+        auto&& avalableBlkCounter = m_pdev_avalable_blk_map.find(pdevID)->second;
+        avalableBlkCounter.fetch_add(available_blk_num);
+    }
+    
+    m_chunks.emplace(vchunk.get_chunk_id(), chunk);
+
+    auto& heapLock = it->second->first;
+    auto& heap = it->second->second;
+
+    std::lock_guard<std::mutex> l(heapLock); 
+    heap.emplace(chunk);
 }
 
 //select_chunk will only be called in homestore when allocating a block
 csharedChunk HeapChunkSelector::select_chunk(homestore::blk_count_t count, const homestore::blk_alloc_hints& hint) {
+    auto& chunkID = hint.chunk_id_hint;
+    if(chunkID != homestore::INVALID_CHUNK_ID) {
+        auto it = m_chunks.find(chunkID);
+        if(it == m_chunks.end()) {
+            // whether we should terminate if this happen?
+            LOGERROR("No chunk found for pg");
+            return nullptr;
+        }
+        auto& vchunk = it->second;
+        return vchunk.get_internal_chunk();
+    }
+
     auto pdevID = hint.dev_id_hint;
     if(pdevID == homestore::INVALID_DEV_ID) {
     // this is the first shard of this pg, select a pdev with the most available blocks for it
-        std::lock_guard<std::mutex> l(lock);
-        auto it = std::max_element(m_pdev_avalable_blk_map.begin(), m_pdev_avalable_blk_map.end(),
+        //std::lock_guard<std::mutex> l(lock);
+        auto&& it = std::max_element(m_pdev_avalable_blk_map.begin(), m_pdev_avalable_blk_map.end(),
         [](const std::pair<uint32_t, uint32_t>& lhs, const std::pair<uint32_t, uint32_t>& rhs) { 
             return lhs.second < rhs.second; }
         );
+        if(it == m_pdev_avalable_blk_map.end()) {
+            LOGINFO("No pdev found for pg");
+            return nullptr;
+        }
         pdevID = it->first;
     }
 
@@ -42,19 +80,21 @@ csharedChunk HeapChunkSelector::select_chunk(homestore::blk_count_t count, const
         return nullptr;
     }
 
-    
-    it->second->first.lock();
-    if(it->second->second.empty()) {
-        LOGINFO("No more available chunks found for pdev {}", pdevID);
-        return nullptr;
-    }
-    auto& vchunk = it->second->second.top();
-    it->second->second.pop();
-    it->second->first.unlock();
-    
-    {
-        std::lock_guard<std::mutex> l(lock);
-        m_pdev_avalable_blk_map[pdevID] -= vchunk.available_blks();
+    const auto& vchunk = [it = std::move(it), pdevID]() {
+        auto& heapLock = it->second->first;
+        auto& heap = it->second->second;
+        std::lock_guard<std::mutex> l(heapLock);
+        if(heap.empty()) return VChunk(nullptr);
+        const auto& vchunk = heap.top();
+        heap.pop();
+        return vchunk;
+    }();
+
+    if(vchunk.get_internal_chunk()){
+        auto&& avalableBlkCounter = m_pdev_avalable_blk_map.find(pdevID)->second;
+        avalableBlkCounter.fetch_sub(vchunk.available_blks());
+    } else {
+        LOGINFO("No pdev found for pdev {}", pdevID);
     }
 
     return vchunk.get_internal_chunk();
@@ -62,13 +102,9 @@ csharedChunk HeapChunkSelector::select_chunk(homestore::blk_count_t count, const
 
 void HeapChunkSelector::foreach_chunks(std::function< void(csharedChunk&) >&& cb) {
     //we should call `cb` on all the chunks, selected or not
-
     //actually, after vdev is initialized, no more new chunks will be added again, 
-    //so we can make this assumption and disable this lock.
-
-    //if we support dynamiclly add new chunks to vdev in the future, we need to enable this lock
-    //std::lock_guard<std::mutex> l(lock); 
+    //so we can make this assumption that when this is called
     std::for_each(std::execution::par_unseq, m_chunks.begin(), m_chunks.end(), 
-    [cb = std::move(cb)](const VChunk& p){ cb(p.get_internal_chunk()); });
+    [cb = std::move(cb)](const std::pair< uint32_t, VChunk >& p){ cb(p.second.get_internal_chunk()); });
 }
 }   // namespace homeobject
--- src/lib/homestore/heap_chunk_selector.h
+++ src/lib/homestore/heap_chunk_selector.h
@@ -11,8 +11,9 @@
 #include <memory>
 #include <map>
 #include <functional>
+#include <atomic>
 
-#include <folly/concurrency/ConcurrentHashMap.h>
+#include <folly/AtomicHashMap.h>
 
 using VChunk = homestore::VChunk;
 
@@ -27,7 +28,8 @@ using csharedChunk = homestore::cshared<homestore::Chunk>;
 
 class HeapChunkSelector : public homestore::ChunkSelector {
 public:
-    HeapChunkSelector() = default;
+    HeapChunkSelector();
+    HeapChunkSelector(const uint32_t&, const uint32_t&, const uint32_t&);
     ~HeapChunkSelector() = default;
 
     class VChunkComparator {
@@ -43,15 +45,19 @@ public:
     void foreach_chunks(std::function< void(csharedChunk&) >&& cb) override;
     csharedChunk select_chunk([[maybe_unused]]homestore::blk_count_t nblks, const homestore::blk_alloc_hints& hints);
 
+    using PdevHeapMap = folly::AtomicHashMap< uint32_t, std::shared_ptr< std::pair< std::mutex, VChunkHeap > > >;
+    using PdevAvalableBlkMap = folly::AtomicHashMap< uint32_t, std::atomic_uint32_t >;
+    using ChunkMap = folly::AtomicHashMap< uint32_t, VChunk >;
+
 private:
-    folly::ConcurrentHashMap< uint32_t, std::shared_ptr< std::pair< std::mutex, VChunkHeap > > > m_pdev_heap_map;
+    //this holds all the chunks for each pdev, they are all unselected.
+    PdevHeapMap m_pdev_heap_map;
+
     //for now, uint32_t is enough for the sum of all the available blocks of a pdev.
     //if necessary , we can change this to uint64_t to hold a larger sum.
-    std::unordered_map< uint32_t, uint32_t> m_pdev_avalable_blk_map;
+    PdevAvalableBlkMap m_pdev_avalable_blk_map;
 
     //hold all the chunks , selected or not
-    std::unordered_set< VChunk > m_chunks;
-    std::mutex lock;
-    
+    ChunkMap m_chunks;
 };
 } // namespace homeobject
