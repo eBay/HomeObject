cf75abc45bef5b93ccff643139dd6c36ae08e7d4
yuwmao <yuwmao@ebay.com>
2024-11-08 12:01:25 +0800

    Fix version

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class HomeObjectConan(ConanFile):
     name = "homeobject"
-    version = "2.1.7"
+    version = "2.1.7-baseline_resync"
 
     homepage = "https://github.com/eBay/HomeObject"
     description = "Blob Store built on HomeReplication"
