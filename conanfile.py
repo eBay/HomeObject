2070ed74cc4adf413636640c9994d8bf40cd50d6
Xiaoxi Chen <xiaoxchen@ebay.com>
2025-05-20 17:57:51 -0700

    bump conan version
    Signed-off-by: Xiaoxi Chen <xiaoxchen@ebay.com>

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class HomeObjectConan(ConanFile):
     name = "homeobject"
-    version = "2.3.21"
+    version = "2.3.22"
 
     homepage = "https://github.com/eBay/HomeObject"
     description = "Blob Store built on HomeReplication"
