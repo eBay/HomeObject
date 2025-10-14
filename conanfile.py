2ec11f6c9aad78b0eb8204ee235415c2ffa7f8fb
Jie Yao <jyao3@ebaychina.com>
2025-10-14 15:27:21 +0800

    improve gc log

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ required_conan_version = ">=1.60.0"
 
 class HomeObjectConan(ConanFile):
     name = "homeobject"
-    version = "2.7.10"
+    version = "2.7.11"
 
     homepage = "https://github.com/eBay/HomeObject"
     description = "Blob Store built on HomeReplication"
