db6f807c07ad880566912066ce49070325e9b325
Jie Yao <jyao3@ebaychina.com>
2025-12-26 17:12:22 +0800

    change gc policy (#380)

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ required_conan_version = ">=1.60.0"
 
 class HomeObjectConan(ConanFile):
     name = "homeobject"
-    version = "4.0.6"
+    version = "4.0.7"
 
     homepage = "https://github.com/eBay/HomeObject"
     description = "Blob Store built on HomeStore"
