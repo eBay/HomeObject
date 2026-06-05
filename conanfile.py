5f757ac197954a2c9a74997466767d6b9f814e39
yawzhang <yawzhang@ebay.com>
2026-06-05 16:00:08 +0800

    dump chunk state in http apis

--- conanfile.py
+++ conanfile.py
@@ -10,7 +10,7 @@ required_conan_version = ">=1.60.0"
 
 class HomeObjectConan(ConanFile):
     name = "homeobject"
-    version = "4.1.16"
+    version = "4.1.17"
 
     homepage = "https://github.com/eBay/HomeObject"
     description = "Blob Store built on HomeStore"
