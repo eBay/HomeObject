2249a811af37c12b7b2b7909ba9701d0834904a0
Hooper <hoolu@ebay.com>
2026-07-08 21:34:11 +0800

    SDSTOR-23030 fix backward compatibility

--- src/lib/homestore_backend/replication_state_machine.cpp
+++ src/lib/homestore_backend/replication_state_machine.cpp
@@ -212,21 +212,14 @@ ReplicationStateMachine::get_blk_alloc_hints(sisl::blob const& header, uint32_t 
     switch (msg_header->msg_type) {
     case ReplicationMessageType::CREATE_SHARD_MSG:
     case ReplicationMessageType::SEAL_SHARD_MSG: {
-        if (data_size == 0) {
-            // New log-only format: should not reach here, but handle gracefully.
-            LOGW("get_blk_alloc_hints called for log-only shard message type={}, shard={}, pg={}", msg_header->msg_type,
-                 msg_header->shard_id, msg_header->pg_id);
-            return homestore::blk_alloc_hints{};
-        } else {
-            // Old format: shard message carried shard header/footer data blocks. Return committed_blk_id
-            // so HomeStore skips block allocation and data write. Shard on_commit will ignore pbas.
-            homestore::blk_alloc_hints hints;
-            hints.committed_blk_id = HSHomeObject::tombstone_pbas;
-            LOGW("get_blk_alloc_hints called for old shard message type={}, shard={}, pg={}, return committed_blk hint "
-                 "to skip blk allocation",
-                 msg_header->msg_type, msg_header->shard_id, msg_header->pg_id);
-            return hints;
-        }
+        // Old format: shard message carried shard header/footer data blocks. Return committed_blk_id
+        // so HomeStore skips block allocation and data write. Shard on_commit will ignore pbas.
+        homestore::blk_alloc_hints hints;
+        hints.committed_blk_id = HSHomeObject::tombstone_pbas;
+        LOGW("get_blk_alloc_hints called for old shard message type={}, shard={}, pg={}, return committed_blk hint "
+             "to skip blk allocation",
+             msg_header->msg_type, msg_header->shard_id, msg_header->pg_id);
+        return hints;
     }
 
     case ReplicationMessageType::PUT_BLOB_MSG:
